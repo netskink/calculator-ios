19f5c26b97b276674906edf41b240cfcb787faf8
John F. Davis <davisjf@gmail.com>
2015-05-03 18:09:51 -0600

    No layout rules, but my layout is not as good as his. Not sure what is wrong.

--- calculator-ios/calculator-ios/ViewController.swift
+++ calculator-ios/calculator-ios/ViewController.swift
@@ -41,5 +41,70 @@ class ViewController: UIViewController {
 //        println("digit = \(digit)")
         
     }
+    
+    var operandStack = Array<Double>()
+
+    // this function has a single argument which
+    // is a function with two double parms and returns a double
+    // Unless this noted as private it will generate an error.  In swift
+    // available at time of standford video this was ok, but now it fails
+    // since the class is inherited from view controller and it is objective-C w
+    // which can not handle this.  If you mark the function as private it 
+    // will not expose the routine to the objective c superclass?
+    //
+    // more info here:
+    // http://stackoverflow.com/questions/29457720/compiler-error-method-with-objective-c-selector-conflicts-with-previous-declara/29670644#29670644
+    //
+    // and here
+    // http://stackoverflow.com/questions/29457720/compiler-error-method-with-objective-c-selector-conflicts-with-previous-declara
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation( operandStack.removeLast(), operandStack.removeLast() )
+            pressEnter()
+        }
+    }
+    // this function has a single argument which
+    // is a function with one double parm and returns a double
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation( operandStack.removeLast() )
+            pressEnter()
+        }
+    }
+    
+    @IBAction func pressOperator(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            pressEnter()
+        }
+        switch operation {
+            case "×": performOperation{ $0 * $1 }
+            case "÷": performOperation{ $1 / $0 }
+            case "+": performOperation{ $0 * $1 }
+            case "−": performOperation{ $1 - $0 }
+            case "√": performOperation{ sqrt($0) }
+            default: break
+            
+            
+        }
+        
+    }
+    
+    @IBAction func pressEnter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            // newValue is the value when someone sets it.
+            display.text = "\(newValue)" // Convert a double to string
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
